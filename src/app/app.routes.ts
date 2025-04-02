1558fa086e4028cd969a12564abb8ebbda2f9ce2
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-04-02 19:29:08 +0200

    feat: created main component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LandingHeaderComponent } from './components/landing-header/landing-header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: LandingHeaderComponent }
+];
