471598a14d6e644e08b53a3eab6b34d896052ed5
DavMunHer <davidmunyozh@gmail.com>
2025-04-15 22:57:25 +0200

    feat: Now showing the dos or playground component according to selection in the component explorer

--- src/app/components/component-view/component-view.component.ts
+++ src/app/components/component-view/component-view.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { CapitalizePipe } from '../../pipes/capitalize.pipe';
 import { ComponentExplorerComponent } from './component-explorer/component-explorer.component';
 import { AuthDocsComponent } from './component-explorer/docs/auth-docs/auth-docs.component';
 import { AuthPlaygroundComponent } from './component-explorer/playgrounds/auth-playground/auth-playground.component';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-component-view',
@@ -12,8 +13,21 @@ import { AuthPlaygroundComponent } from './component-explorer/playgrounds/auth-p
   templateUrl: './component-view.component.html',
   styleUrl: './component-view.component.css'
 })
-export class ComponentViewComponent {
+export class ComponentViewComponent implements OnInit{
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
   protected componentName = this.route.snapshot.params['name'];
-  protected showedInfo: 'docs' | 'playground' = this.route.snapshot.params['showedInfo'];
+  protected showedInfo = signal<'docs' | 'playground'>(this.route.snapshot.params['showedInfo']);
+  
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.onRouteChange();
+    });
+  }
+
+  private onRouteChange(): void {
+    this.showedInfo.set(this.route.snapshot.params['showedInfo']);
+  }
+
+  
 }
