d2079a7fea87d87dc9fa6344a1db012fe0eeb0f7
DavMunHer <davidmunyozh@gmail.com>
2025-04-15 20:10:07 +0200

    feat(style): Added basic styles as a template for the component

--- src/app/components/component-view/component-explorer/component-explorer.component.ts
+++ src/app/components/component-view/component-explorer/component-explorer.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'component-explorer',
-  imports: [],
+  imports: [NgClass, RouterLink],
   templateUrl: './component-explorer.component.html',
   styleUrl: './component-explorer.component.css'
 })
+  
 export class ComponentExplorerComponent {
+  private route = inject(ActivatedRoute);
+  protected componentName = this.route.snapshot.params['name'];
+
+  protected isPlaygroundActive() {
+    return this.route.snapshot.params['showedInfo'] == 'playground'
+  }
+
 
 }
