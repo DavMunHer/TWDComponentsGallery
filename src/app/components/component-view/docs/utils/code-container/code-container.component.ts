a8bb7030f8cbdba7e0bb6c732c994cfc65c0416c
DavMunHer <davidmunyozh@gmail.com>
2025-05-01 22:38:25 +0200

    refactor: moved the code tags in a separated component

--- src/app/components/component-view/docs/utils/code-container/code-container.component.ts
+++ src/app/components/component-view/docs/utils/code-container/code-container.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'code-container',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './code-container.component.html',
-  styleUrl: './code-container.component.css'
+  styleUrl: './code-container.component.css',
 })
 export class CodeContainerComponent {
+  protected copied = signal<boolean>(false);
 
+  copyContent() {
+      // TODO: Logic for copying text to clipboard
+      this.copied.set(true);
+      setTimeout(() => {
+        this.copied.set(false);
+      }, 1500);
+  }
 }
