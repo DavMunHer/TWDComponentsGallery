4e3bb1d8b5da5c68506b1ea629c1372ed9aa837c
DavMunHer <davidmunyozh@gmail.com>
2025-05-01 20:06:53 +0200

    feat: added template for the auth docs component

--- src/app/components/component-view/docs/auth-docs/auth-docs.component.ts
+++ src/app/components/component-view/docs/auth-docs/auth-docs.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CodeContainerComponent } from '../utils/code-container/code-container.component';
 
 @Component({
   selector: 'auth-docs',
-  imports: [],
+  imports: [CodeContainerComponent],
   templateUrl: './auth-docs.component.html',
   styleUrl: './auth-docs.component.css'
 })
---
+++ src/app/components/component-view/docs/utils/code-container/code-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'code-container',
+  imports: [],
+  templateUrl: './code-container.component.html',
+  styleUrl: './code-container.component.css'
+})
+export class CodeContainerComponent {
+
+}
