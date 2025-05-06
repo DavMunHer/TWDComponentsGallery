ab24ea84fc69eba557c7b765fa53f15abe4c1487
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-05-06 20:07:54 +0200

    feat: now the option custom works on the select

--- src/app/components/component-view/playgrounds/auth-playground/auth-playground.component.ts
+++ src/app/components/component-view/playgrounds/auth-playground/auth-playground.component.ts
@@ -10,14 +10,31 @@ import { CommonModule } from '@angular/common';
   styleUrl: './auth-playground.component.css'
 })
 export class AuthPlaygroundComponent {
-  selectedTemplate: "classicB&W" | "neoViolet" | "custom" = 'classicB&W';
+  selectedTemplate: "classicB&W" | "neoViolet" | "custom" = 'custom';
   componentVisible = true;
-  constructor() {
-    setInterval(()=>console.log(this.selectedTemplate), 1500);
-  }
+  primaryColor = '#372aac';
+  secondaryColor = '#ffffff';
+  inputColor = '#e5e7eb';
 
   onTemplateChange(newTemplate: "classicB&W" | "neoViolet" | "custom" = 'classicB&W') {
     this.selectedTemplate = newTemplate;
+
+    if (newTemplate === 'custom') {
+      this.validateCustomColors();
+    }
+
+    this.componentVisible = false;
+    setTimeout(() => this.componentVisible = true, 0);
+  }
+
+  validateCustomColors() {
+    const inputs = document.querySelectorAll<HTMLInputElement>('.color-inputs input');
+    const [primaryInput, secondaryInput, inputInput] = Array.from(inputs);
+
+    this.primaryColor = primaryInput.value.trim();
+    this.secondaryColor = secondaryInput.value.trim();
+    this.inputColor = inputInput.value.trim();
+
     this.componentVisible = false;
     setTimeout(() => this.componentVisible = true, 0);
   }
