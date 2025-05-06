60893ff325a3144e1f32c4210c6a918cf36c90d4
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-05-06 18:36:52 +0200

    feat: the select now works and change the template of the component

--- src/app/components/component-view/playgrounds/auth-playground/auth-playground.component.ts
+++ src/app/components/component-view/playgrounds/auth-playground/auth-playground.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AuthComponent } from "@triwebdev/auth-component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'auth-playground',
-  imports: [AuthComponent],
+  imports: [AuthComponent, FormsModule, CommonModule],
   templateUrl: './auth-playground.component.html',
   styleUrl: './auth-playground.component.css'
 })
 export class AuthPlaygroundComponent {
+  selectedTemplate: "classicB&W" | "neoViolet" | "custom" = 'classicB&W';
+  componentVisible = true;
+  constructor() {
+    setInterval(()=>console.log(this.selectedTemplate), 1500);
+  }
 
+  onTemplateChange(newTemplate: "classicB&W" | "neoViolet" | "custom" = 'classicB&W') {
+    this.selectedTemplate = newTemplate;
+    this.componentVisible = false;
+    setTimeout(() => this.componentVisible = true, 0);
+  }
 }
