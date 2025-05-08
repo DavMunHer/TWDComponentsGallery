9aa94ad0f3b19d1d965943a15f6a2f9312b1ce06
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-05-08 19:02:36 +0200

    refactor: changed variables to signals

--- src/app/components/component-view/playgrounds/auth-playground/auth-playground.component.ts
+++ src/app/components/component-view/playgrounds/auth-playground/auth-playground.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthComponent } from "@triwebdev/auth-component";
 import { CommonModule } from '@angular/common';
@@ -10,32 +10,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './auth-playground.component.css'
 })
 export class AuthPlaygroundComponent {
-  selectedTemplate: "classicB&W" | "neoViolet" | "custom" = 'custom';
-  componentVisible = true;
-  primaryColor = '#372aac';
-  secondaryColor = '#ffffff';
-  inputColor = '#e5e7eb';
+  selectedTemplate: WritableSignal<"classicB&W" | "neoViolet" | "custom"> = signal('custom');
+  componentVisible = signal(true);
+  primaryColor = signal('#372aac');
+  secondaryColor = signal('#ffffff');
+  inputColor = signal('#e5e7eb');
 
   onTemplateChange(newTemplate: "classicB&W" | "neoViolet" | "custom" = 'classicB&W') {
-    this.selectedTemplate = newTemplate;
+    this.selectedTemplate.set(newTemplate);
 
     if (newTemplate === 'custom') {
       this.validateCustomColors();
     }
 
-    this.componentVisible = false;
-    setTimeout(() => this.componentVisible = true, 0);
+    this.componentVisible.set(false);
+    setTimeout(() => this.componentVisible.set(true), 0);
   }
 
   validateCustomColors() {
     const inputs = document.querySelectorAll<HTMLInputElement>('.color-inputs input');
     const [primaryInput, secondaryInput, inputInput] = Array.from(inputs);
 
-    this.primaryColor = primaryInput.value.trim();
-    this.secondaryColor = secondaryInput.value.trim();
-    this.inputColor = inputInput.value.trim();
+    this.primaryColor.set(primaryInput.value.trim());
+    this.secondaryColor.set(secondaryInput.value.trim());
+    this.inputColor.set(inputInput.value.trim());
 
-    this.componentVisible = false;
-    setTimeout(() => this.componentVisible = true, 0);
+    this.componentVisible.set(false);
+    setTimeout(() => this.componentVisible.set(true), 0);
   }
 }
