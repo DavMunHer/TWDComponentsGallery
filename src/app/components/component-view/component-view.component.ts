f6aa9d19c82a481efc8e3c1e2f5aec03e5654bc1
DavMunHer <davidmunyozh@gmail.com>
2025-09-02 21:50:03 +0200

    style: added blur to the right content when showing sidebar

--- src/app/components/component-view/component-view.component.ts
+++ src/app/components/component-view/component-view.component.ts
@@ -2,13 +2,13 @@ import { Component, ElementRef, inject, OnInit, signal, viewChild } from '@angul
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ComponentExplorerComponent } from './component-explorer/component-explorer.component';
-import { AuthDocsComponent } from './docs/auth-docs/auth-docs.component';
 import { AuthPlaygroundComponent } from './playgrounds/auth-playground/auth-playground.component';
 import { ComponentsInfoService } from '../../services/components-info.service';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ResponsiveMenuComponent } from './responsive-menu/responsive-menu.component';
 import { ClickOutsideDirective } from '../../directives/click-outside.directive';
 import { DynamicDocsComponent } from './docs/dynamic-docs/dynamic-docs.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-component-view',
@@ -16,8 +16,9 @@ import { DynamicDocsComponent } from './docs/dynamic-docs/dynamic-docs.component
     ClickOutsideDirective,
     SidebarComponent, ComponentExplorerComponent,
     DynamicDocsComponent, AuthPlaygroundComponent,
-    NavbarComponent, ResponsiveMenuComponent
-  ],
+    NavbarComponent, ResponsiveMenuComponent,
+    NgClass
+],
   templateUrl: './component-view.component.html',
   styleUrl: './component-view.component.css'
 })
