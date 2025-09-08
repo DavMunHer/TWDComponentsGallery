1d5ff705b3719397b5d7e3737b5ce16f3929c47f
DavMunHer <davidmunyozh@gmail.com>
2025-09-08 20:03:08 +0200

    feat: linked forms to their respective ts variables

--- src/app/components/component-view/playgrounds/carousel-playground/carousel-playground.component.ts
+++ src/app/components/component-view/playgrounds/carousel-playground/carousel-playground.component.ts
@@ -1,32 +1,40 @@
 import { Component, signal } from '@angular/core';
-import {CarouselComponent} from '@triwebdev/carousel-component'
-import { RouterLink } from "@angular/router";
+import { CarouselComponent } from '@triwebdev/carousel-component';
+import { RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'carousel-playground',
-  imports: [CarouselComponent, RouterLink],
+  imports: [CarouselComponent, RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './carousel-playground.component.html',
-  styleUrl: './carousel-playground.component.css'
+  styleUrl: './carousel-playground.component.css',
 })
 export class CarouselPlaygroundComponent {
-  protected cards = signal([
-    {
+  protected cards = signal([{}, {}, {}, {}, {}, {}]);
 
-    },
-    {
+  protected maxCardsForm = new FormGroup({
+    maxCards: new FormControl<number>(6, { nonNullable: true }),
+  });
 
-    },
-    {
+  protected cardTemplateForm = new FormGroup({
+    selectedTemplate: new FormControl<'template' | 'custom'>('template', {
+      nonNullable: true,
+    }),
+  });
 
-    },
-    {
+  protected scrollBehaviorForm = new FormGroup({
+    selectedBehavior: new FormControl<'manual' | 'auto'>('manual', {
+      nonNullable: true,
+    }),
+  });
 
-    },
-    {
-
-    },
-    {
-
-    }
-  ])
+  protected autoScrollConfigForm = new FormGroup({
+    firstMoveDelayMultiplier: new FormControl<number>(1.5, { nonNullable: true }),
+    msPerMove: new FormControl<number>(2000, { nonNullable: true }),
+  });
 }
