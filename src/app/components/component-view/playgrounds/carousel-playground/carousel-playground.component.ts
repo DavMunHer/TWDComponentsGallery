0a5db52e0b137a729a4b5412bd8baef56271c7d4
DavMunHer <davidmunyozh@gmail.com>
2025-09-08 17:22:52 +0200

    feat: added playground introduction and adjusted component width to fit the vw

--- src/app/components/component-view/playgrounds/carousel-playground/carousel-playground.component.ts
+++ src/app/components/component-view/playgrounds/carousel-playground/carousel-playground.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import {CarouselComponent} from '@triwebdev/carousel-component'
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'carousel-playground',
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, RouterLink],
   templateUrl: './carousel-playground.component.html',
   styleUrl: './carousel-playground.component.css'
 })
