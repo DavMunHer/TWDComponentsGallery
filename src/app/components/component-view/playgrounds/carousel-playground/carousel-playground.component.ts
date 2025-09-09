602cde3389d45fb95463ce50d8aff35efe701a9f
DavMunHer <davidmunyozh@gmail.com>
2025-09-09 18:35:18 +0200

    feat: implemented logic for changing the behavior of the component according to the form values

--- src/app/components/component-view/playgrounds/carousel-playground/carousel-playground.component.ts
+++ src/app/components/component-view/playgrounds/carousel-playground/carousel-playground.component.ts
@@ -16,25 +16,63 @@ import {
 })
 export class CarouselPlaygroundComponent {
   protected cards = signal([{}, {}, {}, {}, {}, {}]);
+  protected showCarousel = signal<boolean>(true);
+
 
   protected maxCardsForm = new FormGroup({
     maxCards: new FormControl<number>(6, { nonNullable: true }),
   });
+  protected maxCardsSignal = signal<number>(6);
 
   protected cardTemplateForm = new FormGroup({
     selectedTemplate: new FormControl<'template' | 'custom'>('template', {
       nonNullable: true,
     }),
   });
+  protected selectedTemplateSignal = signal<'template' | 'custom'>('template');
+
 
   protected scrollBehaviorForm = new FormGroup({
-    selectedBehavior: new FormControl<'manual' | 'auto'>('manual', {
+    selectedBehavior: new FormControl<'manual-only' | 'auto'>('manual-only', {
       nonNullable: true,
     }),
   });
+  protected scrollBehaviorSignal = signal<'manual-only' | 'auto'>('manual-only');
 
   protected autoScrollConfigForm = new FormGroup({
     firstMoveDelayMultiplier: new FormControl<number>(1.5, { nonNullable: true }),
     msPerMove: new FormControl<number>(2000, { nonNullable: true }),
   });
+  protected autoScrollConfigSignal = signal<any>({firstMoveDelayMultiplier: 1.5, msPerMove:2000});
+
+
+
+  constructor() {
+    this.maxCardsForm.valueChanges.subscribe(value => {
+      this.maxCardsSignal.set(value.maxCards!);
+      this.reloadCarousel();
+    })
+
+    this.cardTemplateForm.valueChanges.subscribe(value => {
+      this.selectedTemplateSignal.set(value.selectedTemplate!);
+      this.reloadCarousel();
+    })
+
+    this.scrollBehaviorForm.valueChanges.subscribe(value => {
+      this.scrollBehaviorSignal.set(value.selectedBehavior!);
+      this.reloadCarousel();
+    })
+
+    this.autoScrollConfigForm.valueChanges.subscribe(value => {
+      this.autoScrollConfigSignal.set(value);
+      this.reloadCarousel();
+    })
+  }
+
+
+  private reloadCarousel() {
+    this.showCarousel.set(false);
+    setTimeout(() => this.showCarousel.set(true), 100);
+  }
+
 }
